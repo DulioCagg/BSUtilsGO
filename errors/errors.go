e69f237a1de5c195d0d30e8fd576b23340229454
DulioCagg <duliop.caggiano@hotmail.com>
2020-10-20 10:51:08 -0500

    First commit

---
+++ errors/errors.go
@@ -0,0 +1,44 @@
+package errors
+
+import "net/http"
+
+// RestErr is used to stablish a common error response throughout the application
+type RestErr struct {
+	Message string   `json:"message"`
+	Status  int      `json:"status"`
+	Type    string   `json:"type"`
+	Causes  []string `json:"causes"`
+}
+
+// BadRequest creates a RestErr with the given message
+func BadRequest(message string) *RestErr {
+	return &RestErr{
+		Message: message,
+		Status:  http.StatusBadRequest,
+		Type:    "bad_request",
+	}
+}
+
+// NotFound creates a RestErr with the given message
+func NotFound(message string) *RestErr {
+	return &RestErr{
+		Message: message,
+		Status:  http.StatusNotFound,
+		Type:    "not_found",
+	}
+}
+
+// InternalServerError creates a RestErr with the given message
+func InternalServerError(message string, err error) *RestErr {
+	result := &RestErr{
+		Message: message,
+		Status:  http.StatusInternalServerError,
+		Type:    "internal_server_error",
+	}
+
+	if err != nil {
+		result.Causes = append(result.Causes, err.Error())
+	}
+
+	return result
+}
---
+++ errors/errors_test.go
@@ -0,0 +1,38 @@
+package errors
+
+import (
+	"errors"
+	"net/http"
+	"testing"
+
+	"github.com/matryer/is"
+)
+
+func TestInternalServer(t *testing.T) {
+	is := is.New(t)
+	err := InternalServerError("message", errors.New("database error"))
+
+	is.Equal(err.Status, http.StatusInternalServerError)
+	is.Equal(err.Message, "message")
+	is.Equal(err.Type, "internal_server_error")
+
+	is.True(err.Causes != nil)
+	is.Equal(len(err.Causes), 1)
+	is.Equal(err.Causes[0], "database error")
+}
+
+func TestNotFound(t *testing.T) {
+	is := is.New(t)
+	err := NotFound("message")
+	is.Equal(err.Status, http.StatusNotFound)
+	is.Equal(err.Message, "message")
+	is.Equal(err.Type, "not_found")
+}
+
+func TestBadRequest(t *testing.T) {
+	is := is.New(t)
+	err := BadRequest("message")
+	is.Equal(err.Status, http.StatusBadRequest)
+	is.Equal(err.Message, "message")
+	is.Equal(err.Type, "bad_request")
+}
